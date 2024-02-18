a46e2708cb5e8676b0858d20d4c4f2141153e5c3
mohamedali209 <mohamedali389104@gmail.com>
2024-02-18 23:30:44 +0200

    update

--- lib/pages/update.dart
+++ lib/pages/update.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class update extends StatelessWidget {
-  update({
-    this.dollar2,
-    super.key});
+  update({this.dollar2, super.key});
   double? dollar2;
   static String id = 'update';
   TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Color.fromARGB(255, 11, 38, 60),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 11, 38, 60),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextFormField(
             controller: controller,
             keyboardType: TextInputType.number,
-            decoration:const InputDecoration(
-                hintText: 'dollar', border: OutlineInputBorder()),
+            decoration: const InputDecoration(
+                focusColor: Colors.amber,
+                hintText: 'dollar',
+                hintStyle: TextStyle(color: Colors.amber),
+                
+                
+                border: OutlineInputBorder(
+                  
+                  borderSide: BorderSide(color: Colors.amber),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber))),
             onChanged: (value) {
               dollar2 = double.parse(value);
             },
           ),
-          IconButton(
-              onPressed: () {
-                Navigator.pop(context, dollar2);
-              },
-              icon:const Icon(Icons.update))
+          IconButton(onPressed: () {}, icon: const Icon(Icons.update))
         ],
       ),
     );
